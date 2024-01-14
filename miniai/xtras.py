3ea46f42d55aeaedfb304be5843712064bd7cfa6
Jay Suh <shipalnomoo@gmail.com>
2024-01-13 21:47:51 -0600

    Plot losses

--- miniai/xtras.py
+++ miniai/xtras.py
@@ -8,6 +8,7 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 import fastcore.all as fc
 import matplotlib.pyplot as plt
+from fastprogress import progress_bar,master_bar
 
 from .datasets import *
 from .learner import *
@@ -108,3 +109,49 @@ class MyNorm(nn.Module):
         std  = x.std((0,2,3), keepdim=True)
         x = (x - mean) / (std+self.eps).sqrt()
         return (x * self.mult) + self.add
+
+class ProgressCB(Callback):
+    """
+    Plot frequency options:
+    batch: every batch
+    epoch: every epoch
+    fit: first two epochs and after fit
+    """
+    order = MetricsCB.order+1
+    def __init__(self, plot=False, update_freq='fit', epoch_nums=2): fc.store_attr()
+    def before_fit(self, learn):
+        learn.epochs = self.mbar = master_bar(learn.epochs)
+        self.first = True
+        if hasattr(learn, 'metrics'): learn.metrics._log = self._log
+        self.losses = []
+        self.val_losses = []
+
+    def _log(self, d):
+        if self.first:
+            self.mbar.write(list(d), table=True)
+            self.first = False
+        self.mbar.write(list(d.values()), table=True)
+        
+    def _update_graph(self, learn, valid):
+        if not self.losses: return
+        n = int(valid)
+        self.mbar.update_graph(
+            [[fc.L.range(self.losses), self.losses],
+            [fc.L.range(learn.epoch+n).map(lambda x: (x+1)*len(learn.dls.train)), self.val_losses]])
+
+    def before_epoch(self, learn): learn.dl = progress_bar(learn.dl, leave=False, parent=self.mbar)
+    def after_batch(self, learn):
+        learn.dl.comment = f'{learn.loss:.3f}'
+        if self.plot and hasattr(learn, 'metrics') and learn.training:
+            self.losses.append(learn.loss.item())
+            if self.val_losses and self.update_freq=='batch': self._update_graph(learn, valid=False)
+    
+    def after_epoch(self, learn): 
+        if not learn.training:
+            if self.plot and hasattr(learn, 'metrics'): 
+                self.val_losses.append(learn.metrics.all_metrics['loss'].compute())
+                if self.update_freq=='epoch' or (self.update_freq=='fit' and learn.epoch<self.epoch_nums): 
+                    self._update_graph(learn, valid=True)
+
+    def after_fit(self, learn):
+        if self.plot and self.update_freq == 'fit': self._update_graph(learn, valid=True)
